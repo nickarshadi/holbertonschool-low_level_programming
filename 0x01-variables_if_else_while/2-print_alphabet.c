0199091bf38b88db653d8d5c98697f78d19a6aec
nickarshadi <1477@holbertonschool.com>
2020-02-06 14:42:52 -0500

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,20 @@
 int main(void)
 {
 char c = 'a';
-int i=0;
-while(i<24)
+int i = 0;
+while (c == 'a' || c == 'A')
+{
+while (i < 25)
 {
 	putchar(c);
 	c++;
 	i++;
+}
+i = 0;
+if (c == 'z')
+{
+c = 'A';
+}
 }
 putchar('\n');
 return (0);
