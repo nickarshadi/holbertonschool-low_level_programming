2bcd17b7c487a560ed9e27e4f155fc5968ec8d35
nickarshadi <1477@holbertonschool.com>
2020-02-06 14:55:12 -0500

    yes baby

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,19 +7,11 @@ int main(void)
 {
 char c = 'a';
 int i = 0;
-while (c == 'a' || c == 'A')
-{
 while (i <= 25)
 {
 	putchar(c);
 	c++;
 	i++;
-}
-i = 0;
-if (c == '{')
-{
-c = 'A';
-}
 }
 putchar('\n');
 return (0);
