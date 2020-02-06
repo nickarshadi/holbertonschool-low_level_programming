e80949ab86d87c0462b9a9d6091637e6cb6e4eed
nickarshadi <1477@holbertonschool.com>
2020-02-05 21:51:13 -0500

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print the sizes
+ * Return:0
+ */
+int main(void)
+{
+	int schar;
+	int sint;
+	int slongint;
+	int sfloat;
+
+	schar = sizeof(char);
+	sint = sizeof(int);
+	slongint = sizeof(long int);
+	sfloat + sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", schar);
+	printf("Size of an int: %d byte(s)\n", sint);
+	printf("Size of a long int: %d byte(s)\n", slongint);
+	printf("Size of a float: %d byte(s)\n", sfloat);
+
+	return (0);
+}
