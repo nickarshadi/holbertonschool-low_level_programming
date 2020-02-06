e127e86c14fe431c60bdabf914aa6c3322610e7d
nickarshadi <1477@holbertonschool.com>
2020-02-06 17:19:36 -0500

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ * main - print function
+ * Return:0
+ */
+int main(void)
+{
+int i=0;
+char c= 'a';
+while (i < 10)
+{
+	putchar(i+'0');
+	i++;
+}
+i=0;
+while (i < 6)
+{
+	putchar(c);
+	i++;
+	c++;
+}
+putchar('\n');
+return (0);
+}
