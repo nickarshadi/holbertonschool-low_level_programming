8c821902da839eef1f4c8d0025d9a530638ada9e
nickarshadi <1477@holbertonschool.com>
2020-02-06 17:50:38 -0500

    add newline

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@ int main(void)
 int i = 0;
 int z = 9;
 
-while (i < 9)
+while (i < 10)
 {
 	putchar(i + '0');
+	if (i != 9)
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	i++;
 }
-putchar(z + '0');
+putchar('\n');
 return (0);
 }
