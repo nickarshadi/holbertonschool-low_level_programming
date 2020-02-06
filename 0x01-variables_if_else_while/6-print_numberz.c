daea997d1bdcaea7bbfb93bf1189c7023650721e
nickarshadi <1477@holbertonschool.com>
2020-02-06 16:34:01 -0500

    number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print function
+ * Return:0
+ */
+int main(void)
+{
+int i=0;
+int z;
+while (i<10)
+{
+	putchar(i+'0');
+	i++;
+}
+putchar('\n');
+return (0);
+}
