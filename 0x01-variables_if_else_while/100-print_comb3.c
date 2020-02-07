af1054d2f3bc779026e84e0c62275ebf57b4a534
nickarshadi <1477@holbertonschool.com>
2020-02-06 20:44:56 -0500

    change condition

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print function
+ * Return:0
+ */
+int main(void)
+{
+int i = 0;
+int j = 0;
+
+while (i <= 9)
+{
+	j = i+1;
+	while (j < 10)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i != 8 || j != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		j++;
+
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
