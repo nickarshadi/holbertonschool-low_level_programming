881f8d19a8d2bc318441e05267140f737e7704c7
nickarshadi <1477@holbertonschool.com>
2020-02-11 13:33:00 -0500

    add function is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * int _islower
+ * Return: 1 if lowercase else 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
