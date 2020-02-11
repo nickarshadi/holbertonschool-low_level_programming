66a73b5686881e0ceed994e5bac52d6d9e3009f3
nickarshadi <1477@holbertonschool.com>
2020-02-11 14:03:17 -0500

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - print a sign
+ * Return: return value depends on variable
+ * @n: function parameter
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
