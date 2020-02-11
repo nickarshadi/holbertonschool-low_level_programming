1cbabf7809dd32e0a6fb854b779c2e42aec8c722
nickarshadi <1477@holbertonschool.com>
2020-02-11 17:32:57 -0500

    count to 24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+
+	while (a <= 2)
+	{
+		b = 0;
+		
+		while (b <= 9)
+		{
+			c = 0;
+			while (c <= 5)
+			{
+				d = 0;
+				while (d <= 9)
+				{
+					
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+					d++;
+					if (a == 2 && b == 3 && c == 5 && d == 9)
+					{
+						a = 10;
+					       	b = 10;
+						c = 10;
+						d = 11;
+					}
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
