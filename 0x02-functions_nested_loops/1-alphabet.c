1640efc4153bc31d6b9e251c593381ec826421ad
nickarshadi <1477@holbertonschool.com>
2020-02-11 11:09:50 -0500

    update header

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,7 @@
-#include <unistd.h>
 #include "holberton.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - print
+ * Return: nothing
  */
 void print_alphabet(void)
 {
