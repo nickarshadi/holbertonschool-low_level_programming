97cec391343744a53ddcc0d76f5272df3cff6872
nickarshadi <1477@holbertonschool.com>
2020-02-11 13:37:17 -0500

    add description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 /**
- * int _islower
+ * _islower - check if number is lowercase
  * Return: 1 if lowercase else 0
+ * @c: variable
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
