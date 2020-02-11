8edd56bf95685d962dc2557c8efa1ab5eb7823f3
nickarshadi <1477@holbertonschool.com>
2020-02-11 15:02:24 -0500

    recompile files

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,6 +9,8 @@ int i = 0;
 int j = 0;
 int k = 0;
 int n = 0;
+int first;
+int second;
 
 while (i <= 9)
 {
@@ -21,15 +23,30 @@ while (i <= 9)
 			n = 0;
 			while (n < 10)
 			{
+			unsigned concatenate1(unsigned i, unsigned j) {
+    			unsigned pow = 10;
+    			while(j >= pow)
+        		pow *= 10;
+    			return i * pow + j;        
+			}
+			unsigned concatenate2(unsigned k, unsigned n) {
+                        unsigned pow = 10;
+                        while(n >= pow)
+                        pow *= 10;
+                        return k * pow + n;
+                        }
+			if (concatenate1 != concatenate2)
+			{
 			putchar(i + '0');
 			putchar(j + '0');
 			putchar(' ');
 			putchar(k + '0');
 			putchar(n + '0');
-			if (i != 9 || j != 9 || k != 9 || n != 9)
+			if (i != 9 || j != 8 || k != 9 || n != 9)
 			{
 			putchar(',');
 			putchar(' ');
+			}
 			}
 			n++;
 			}
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 /**
- * main - print
- * Return:0
- */
+ *  * main - print
+ *   * Return:0
+ *    */
 int main(void)
 {
 char c = 'z';
 int i = 25;
 while (i >= 0)
 {
-	putchar(c);
-	c--;
-	i--;
+putchar(c);
+c--;
+i--;
 }
 putchar('\n');
 return (0);
