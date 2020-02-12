db8b95d8374f3e2ebded29ee4624cbd9fa3c69f9
nickarshadi <1477@holbertonschool.com>
2020-02-11 22:17:10 -0500

    counting to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,22 @@
-#import "holberton.h"
-#import <stdio.h>
+#include <stdio.h>
+#include "holberton.h"
 /**
  * print_to_98 - print all
- *
- *
+ * Return: no value
+ * @n: variable
  */
 void print_to_98(int n)
 {
 
 	while (n <= 98)
-	{
-		printf("&d, ", n);
+	{       if (n == 98)
+		{
+		printf("%d \n ", n);
+		}
+		else
+		{
+		printf("%d, ", n);
+		}
 		n++;
 	}
 }
