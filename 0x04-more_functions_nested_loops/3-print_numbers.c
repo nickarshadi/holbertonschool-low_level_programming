d7e4d6fa7ceb2ec2accae63ef11a15e68d2e4885
nickarshadi <1477@holbertonschool.com>
2020-02-13 14:53:03 -0500

    count to 10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers
+ * Return: no return value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_numbers
+ * Return: no return value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <10; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
