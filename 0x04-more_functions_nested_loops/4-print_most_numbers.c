1456a0a1b55b6542ab1478c9c1b9cc75332b5298
nickarshadi <1477@holbertonschool.com>
2020-02-13 14:58:52 -0500

    repeat commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 /**
- * print_numbers
+ * print_most_numbers - print numbers from 0 - 10 except 2 and 4
  * Return: no return value
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <10; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i == 2 || i == 4)
+		{
+		}
+		else
 		{
 			_putchar(i + '0');
 		}
