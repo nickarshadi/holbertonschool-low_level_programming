51fa7054bbc11d53f5056d389d8feec15b732fb9
nickarshadi <1477@holbertonschool.com>
2020-02-13 16:48:59 -0500

    create a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print diagonal line with size n
+ * Return: 0
+ * @n
+ */
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b <= c; b++)
+		{
+			_putchar(' ');
+		}
+		c++;
+		_putchar('\\');
+		_putchar('\n');
+	}
+	}
+}
