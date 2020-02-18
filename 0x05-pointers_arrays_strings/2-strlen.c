442822bf901e2b501abc5ef1d8f996bd8cd44cc5
nickarshadi <1477@holbertonschool.com>
2020-02-18 15:51:01 -0500

    count string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - print size of string parameter
+ * Return: length of string
+ *
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+	while (*(s + a) != '\0')
+	{
+		a++;
+	}
+	return(a);
+}
