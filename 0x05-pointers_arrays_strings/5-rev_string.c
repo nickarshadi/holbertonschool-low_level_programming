cc77e3e0822b04b101c88a75cba5ff36ff03d05f
nickarshadi <1477@holbertonschool.com>
2020-02-18 20:31:00 -0500

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * rev_string - main
+ * @s: string
+ *
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+	char c[100];
+	int d = 0;
+
+	while (s[a] != '\0')
+	{
+		c[a] = s[a];
+		a++;
+	}
+	c[a] = '\0';
+	d = a;
+	for (b = 0; b < d; b++)
+	{
+		s[b] = c[a-1];
+		a--;
+	}
+
+}
