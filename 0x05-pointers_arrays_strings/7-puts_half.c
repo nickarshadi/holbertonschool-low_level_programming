362a5db947843d7aaec95eb3e93d97fc74297cd5
nickarshadi <1477@holbertonschool.com>
2020-02-18 22:18:54 -0500

    print second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * puts_half - print half array
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = a / 2;
+	}else
+	{
+		b = (a / 2) + 1;
+	}
+	for (c = b; c <= a; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
