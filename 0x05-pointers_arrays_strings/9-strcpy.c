a4f38ee447cd269b63dce40c716bceffebd1d505
nickarshadi <1477@holbertonschool.com>
2020-02-18 23:31:13 -0500

    strp

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcpy - copy string
+ * @dest: dest
+ * @src: parameter
+ * Return: char dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != 0)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (dest);
+}
