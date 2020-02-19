124bcd6e61b39f7d611baaed403ac4fe21f18edc
nickarshadi <1477@holbertonschool.com>
2020-02-18 22:27:22 -0500

    chane loop

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,6 @@ void puts_half(char *str)
 {
 	int a = 0;
 	int b = 0;
-	int c = 0;
 
 	while (str[a] != '\0')
 	{
@@ -21,9 +20,10 @@ void puts_half(char *str)
 	{
 		b = (a -1) / 2;
 	}
-	for (c = b; c <= a; c++)
+	while (str[b]!= '\0')
 	{
-		_putchar(str[c]);
+		_putchar(str[b]);
+		b++;
 	}
 	_putchar('\n');
 }
