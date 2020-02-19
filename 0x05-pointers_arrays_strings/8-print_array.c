8a371fd041897bd21af35e5f2a9098d6c718ca5f
nickarshadi <1477@holbertonschool.com>
2020-02-18 23:05:44 -0500

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print n elements of an array
+ * @a: array
+ * @n: number
+ */
+void print_array(int *a, int n)
+{
+	int b = 0;
+
+	for (b = 0; b < n; b++)
+	{
+		if (b != n - 1)
+		{
+			printf("%d, ", a[b]);
+		}
+		else
+		{
+			printf("%d", a[b]);
+		}
+	}
+	putchar('\n');
+}
