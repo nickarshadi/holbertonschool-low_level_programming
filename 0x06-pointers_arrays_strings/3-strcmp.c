96082bfea08a70c9ffd3c5a5fe5be79dec607c65
nickarshadi <1477@holbertonschool.com>
2020-02-20 20:24:11 -0500

    check two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strcm - check two string
+ * @s1: var1
+ * @s2: var2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int div = 0;
+
+	for (; s1[a] != '\0' && s2[a] != '\0' && s1[a] == s2[a]; a++)
+	{}
+	div = s1[a] - s2[a];
+	return (div);
+}
