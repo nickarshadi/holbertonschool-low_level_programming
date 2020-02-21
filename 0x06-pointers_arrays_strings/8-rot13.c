0482dfa7db8030994cc5ebc2e948bbd35eabfc0d
nickarshadi <1477@holbertonschool.com>
2020-02-20 23:31:09 -0500

    finiish project

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * rot13 - rot13 encoding
+ * @s: string
+ *
+ * Return: string with rot13;
+ */
+
+char *rot13(char *s)
+{
+        int i;
+        i = 0;
+
+        while (s[i] != '\0')
+        {
+                if (s[i] >= 65 && s[i] <= 77)
+                {
+                        s[i] = s[i] + 13;
+                }
+                else if (s[i] >= 78 && s[i] <= 90)
+                {
+                        s[i] = s[i] -13;
+                }
+                else if (s[i] >= 97 && s[i] <= 109)
+                {
+                        s[i] = s[i] + 13;
+                }
+                else if (s[i] >= 98 && s[i] <= 122)
+                {
+                        s[i] = s[i] -13;
+                }
+                i++;
+        }
+        return (s);
+}
