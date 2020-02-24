ef0c6444b8db92977d582615067e9909603d16c7
nickarshadi <1477@holbertonschool.com>
2020-02-24 18:58:24 +0000

    create memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memset - store b in s
+ * @s: pointer to memeory
+ * @b: character to fill in
+ * @n: integer n
+ * Return: return adress of string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x = 0;
+
+	for (x = 0; x <= n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+}
