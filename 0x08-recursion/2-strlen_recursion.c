4053389923cdd1111de8443685a051ce8c29e40a
nickarshadi <1477@holbertonschool.com>
2020-02-26 15:35:32 -0500

    check length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: recurse string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
