ea92859940de34de5d5d51b5d02c2546ab6c1b02
nickarshadi <1477@holbertonschool.com>
2020-02-26 14:14:54 -0500

    commit task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - recursive string output
+ * @s: character
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
