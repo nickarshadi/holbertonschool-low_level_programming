2b2d1befb45336d88a32f8f843beeb3739b6f783
nickarshadi <1477@holbertonschool.com>
2020-02-26 19:22:37 -0500

    find prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * is_prime_number - check if number is prime number
+ * @n: number
+ * Return: 1 or zero
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+	{
+		return (0);
+	}
+	return (prime(n, 2));
+}
+/**
+ * prime - check for prime number
+ * @n: number
+ * @x: integer
+ * Return: primenumber yes or no
+ */
+int prime(int n, int x)
+{
+	int y = n % x;
+
+	if (x >= n)
+	{
+		return (1);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(n, x + 1));
+	}
+}
