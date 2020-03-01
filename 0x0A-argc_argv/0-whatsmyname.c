51f758f1819d559f6e67acd49243fad10354ee87
nickarshadi <1477@holbertonschool.com>
2020-03-01 16:10:56 -0500

    printing out argument string

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print the programs name, followed by a new line
+ * @argc: amount of arguments
+ * @argv: Array of strings of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}
