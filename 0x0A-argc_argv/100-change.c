ab97f316fbf2c0bfb9923cdfb12a4e41152e0051
nickarshadi <1477@holbertonschool.com>
2020-03-01 23:05:21 -0500

    add change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * main - get the minimum amount of coins 25, 10, 5, 2 ,1 
+ * @argc: amount of arguments
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	return (0);
+}
