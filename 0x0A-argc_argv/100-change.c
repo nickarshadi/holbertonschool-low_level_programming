6bc80ad3b2f80a9457fce2ad4ce4dc7fa8ac3868
nickarshadi <1477@holbertonschool.com>
2020-03-01 23:26:08 -0500

    betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,14 +2,50 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * main - get the minimum amount of coins 25, 10, 5, 2 ,1 
+ * main - get the minimum amount of coins 25, 10, 5, 2 ,1
  * @argc: amount of arguments
  * @argv: array of strings
  * Return: 0
  */
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	int a = 0; /* money */
+	int b = 0; /* times */
+	int c = 0; /* storage*/
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		putchar('0');
+		putchar('\n');
+		return (0);
+	}
+	b = a / 25;
+	if (a % 25 != 0)
+	{
+		c = a % 25;
+		b = b + c / 10;
+		if (c % 10 != 0)
+		{
+			a = c % 10;
+			b = b + a / 5;
+			if (a % 5 != 0)
+			{
+				c = a % 5;
+				b = b + c / 2;
+				if (c % 2 != 0)
+				{
+					a = c % 2;
+					b = b + a / 1;
+				}
+			}
+		}
+	}
+	printf("%d\n", b);
 	return (0);
 }
