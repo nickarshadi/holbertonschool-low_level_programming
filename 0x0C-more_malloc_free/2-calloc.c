2dc3471febf72245bfb3756f3846ab26ae516cd5
nickarshadi <1477@holbertonschool.com>
2020-03-05 15:53:09 -0500

    change datatype

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
+	int  *ar;
 	unsigned int a;
 
 	if (nmemb == 0 || size == 0)
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (a = 0; a < size; a++)
+	for (a = 0; a <= size; a++)
 	{
 		ar[a] = 0;
 	}
