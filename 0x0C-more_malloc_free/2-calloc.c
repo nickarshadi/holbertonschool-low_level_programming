ce45b9a0c4d887edb0b7dfc08043a073cb77843e
nickarshadi <1477@holbertonschool.com>
2020-03-05 15:44:24 -0500

    create array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory for array and set all to NULL
+ * @nmemb: number of elements
+ * @size: amount of bytes per element
+ * Return: null or initiialised array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ar = malloc(nmemb * size);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < size; a++)
+	{
+		ar[a] = 0;
+	}
+	return (ar);
+}
