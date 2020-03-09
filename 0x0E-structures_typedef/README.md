86ba14acc002b1424b7d312905e4643780084617
nickarshadi <1477@holbertonschool.com>
2020-03-09 19:38:42 -0400

    create dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+/home/nick/rep/holbertonschool-low_level_programming/0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: characteristics of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
