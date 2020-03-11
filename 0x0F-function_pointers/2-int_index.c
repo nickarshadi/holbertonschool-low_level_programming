ad16850fe054ea25ebeb6a097b52034e5fc1d258
nickarshadi <1477@holbertonschool.com>
2020-03-11 19:02:54 -0400

    check first occurence of 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - write first coincidence of function != 0
+ * @array: array of integers to be checked
+ * @size: arraysize
+ * @cmp: function to check if 0
+ * Return: first occurence
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array && size && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (a = 0; a < size; a++)
+		{
+			if (cmp(array[a]) != 0)
+			{
+				return (a);
+			}
+		}
+		return (0);
+	}
+	return (0);
+}
