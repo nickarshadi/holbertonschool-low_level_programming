d38f93a36bb7cdf4e4e4829380003d8a29df68e8
nickarshadi <1477@holbertonschool.com>
2020-03-11 22:53:56 -0400

    remove whitespace

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,7 +5,7 @@
  * main - calc num1 operator num2
  * @argc: number of arguments
  * @argv: array of arguments
- * return: 1
+ * Return: 1
  * program prints result followed by newline
  * all numbers stored in int
  * operator none of the 5 print Error exit status 99
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
 	int a = atoi(argv[1]);
 	int b = atoi(argv[3]);
 	char *c = argv[2];
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -28,6 +29,6 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 
-	printf("%i\n", get_op_func(c)(a,b));
+	printf("%i\n", get_op_func(c)(a, b));
 	return (1);
 }
