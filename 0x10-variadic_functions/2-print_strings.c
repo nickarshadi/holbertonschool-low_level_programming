f0c9cffe6a95295c0c710e6215801ffcfe04f11e
nickarshadi <1477@holbertonschool.com>
2020-03-14 17:57:58 -0500

    add condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			temp = "(nil)";
 		}
 		printf("%s", temp);
-		if (a != n - 1 && separator != NULL)
+		if (a != n - 1 && separator != 0)
 		{
 			printf("%s ", separator);
 		}
