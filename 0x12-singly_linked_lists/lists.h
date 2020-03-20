17af14143545897f39e1f091f8a2f6bf64f7b35d
nickarshadi <1477@holbertonschool.com>
2020-03-19 23:41:47 -0500

    correct header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,18 +1,29 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-typedef struct list_holb{
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
 	char *str;
-	int len;
-	struct list_holb *next;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_list(const list_t *);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
 void free_list(list_t *);
 void tortoise(void) __attribute__ ((constructor));
 
-#endif
+#endif /*end _LIST_H_*/
