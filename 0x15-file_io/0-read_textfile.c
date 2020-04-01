85faf8ef143c36b64a8332930cc5d3e9d330aea4
nickarshadi <1477@holbertonschool.com>
2020-04-01 00:31:26 -0500

    Add edge cases to task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,10 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+	char buf[1024 * 8];
 
-	if (!filename || !letters)
+	if (!*filename | !letters)
+	{
 		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
