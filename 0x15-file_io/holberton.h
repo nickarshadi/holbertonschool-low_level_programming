7376bfd4d386919218c6c1afc90dabd6186b8e98
nickarshadi <1477@holbertonschool.com>
2020-03-31 22:38:27 -0500

    Add all tasks of todays project

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _HOLB_H_
+#define _HOLB_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+#define READ_BUF_SIZE 1024
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
