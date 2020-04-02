68f0696f537a9988b96e0a42f26c165d3c0f66b6
Nick <1477@holbertonschool.com>
2020-04-01 22:28:34 -0500

    refresh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -40,7 +40,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IWUSR | S_IRUSR);
-	if (fd == (-1))
+	if (fd == -1)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
