3bb23629506c29fb630f6033fa75b6efb4cb8f05
Nick <1477@holbertonschool.com>
2020-04-01 22:17:59 -0500

    Fix close condition on task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,6 +42,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
-	close (fd);
+	close(fd);
 	return (bytes == len ? 1 : -1);
 }
