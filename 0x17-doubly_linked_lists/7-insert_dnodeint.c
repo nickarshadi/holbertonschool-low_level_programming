95372f84fa4ff52d76c9a3393170ab02796f0e69
nickarshadi <1477@holbertonschool.com>
2020-05-02 20:43:50 +0000

    Free

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (!h || !new)
-		return (new != NULL ? free(new), NULL : NULL);
+		return (new ? free(new), NULL : NULL);
 	new->next = NULL;
 	new->prev = NULL;
 	new->n = n;
