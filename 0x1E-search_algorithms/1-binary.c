e24b8cb762c887a766571751b86ad3f8b7f6d872
nickarshadi <1477@holbertonschool.com>
2021-01-14 19:44:27 +0000

    Fix printed output.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -33,23 +33,24 @@ void print_array(int *array, size_t size)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l, m, r = 0;
+	size_t l, m, r;
 
-	if (!array || size <= 0)
-		return (-1);
-
-	r = size - 1;
-	print_array(array, r + 1);
-	while (l < r)
+	if (array != NULL && size > 0)
 	{
-		m = (l + r) / 2;
-		if (array[m] < value)
-			l = m + 1;
-		else if (array[m] > value)
-			r = m;
-		else
-			return (m);
+		l = 0;
+		r = size - 1;
 		print_array(array + l, r + 1 - l);
+		while (l < r)
+		{
+			m = (l + r) / 2;
+			if (array[m] < value)
+				l = m + 1;
+			else if (array[m] > value)
+				r = m;
+			else
+				return (m);
+			print_array(array + l, r + 1 - l);
+		}
 	}
 	return (-1);
 }
